bc857bda61b7f3e9ab90eeeb7b93fa163bbf2758
Christyan-Cyan <christyananugrah2801@gmail.com>
2021-04-25 21:29:20 +0700

    Menambahkan Fitur Baru

--- rpg/bikin_rpg.py
+++ rpg/bikin_rpg.py
@@ -1,37 +1,4 @@
 import random
-# enemy_hp = 100
-
-# while enemy_hp > 0:
-#     inp_data = input("Attack ? ")
-#     if inp_data == "yes":
-#         enemy_hp -= 20
-#         print("HP Enemy : ", enemy_hp)
-#     elif inp_data == "no":
-#         print("HP Enemy : ", enemy_hp)
-#         break
-#     else:
-#         print("cuih")
-
-# else:
-#     print("Enemy Death")
-
-
-# my_hp = 10
-
-# while my_hp >= 10:
-#     masuk_data = input("Heal ?")
-#     if masuk_data == "yes":
-#         my_hp += 10
-#         print("My HP : ", my_hp)
-#     elif masuk_data == "no":
-#         print("My HP : ", my_hp)
-#         break
-#     else:
-#         print("sssshhhh cring")
-
-# else:
-#     print("Yeeee")
-
 
 my_health = 100
 enemy_health = 100
